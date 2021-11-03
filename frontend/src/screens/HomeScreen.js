c533af5ea147979080652eb7b1479d023076deb6
burcuicen <burcuicen2000@gmail.com>
2021-11-04 02:46:07 +0300

    server fix

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react'
 
-import Product from "../components/Product";
-import LoadingBox from "../components/LoadingBox";
-import MessageBox from "../components/MessageBox";
-import { listProducts } from "../actions/productActions";
-import { useDispatch, useSelector } from "react-redux";
+import Product from '../components/Product'
+import LoadingBox from '../components/LoadingBox'
+import MessageBox from '../components/MessageBox'
+import { listProducts } from '../actions/productActions'
+import { useDispatch, useSelector } from 'react-redux'
 
 export default function HomeScreen() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   //we can use react redux easily other than using ajax requests
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
   //dispatch the action
   useEffect(() => {
     //using react redux to send request is easier than using ajax
-    dispatch(listProducts());
-  }, []);
+    dispatch(listProducts())
+  }, [])
   //check if any error exists, if not return products
   return (
     <div>
       <div class="row">
         <img
           class="headerimg"
-          src="https://i.ibb.co/mTB4LKy/Venus-Jewelry-1.png"
+          src="https://beeimg.com/images/t35506342762.png
+          "
+          alt="header"
         ></img>
       </div>
       <div>
@@ -40,5 +42,5 @@ export default function HomeScreen() {
         </div>
       )}
     </div>
-  );
+  )
 }
